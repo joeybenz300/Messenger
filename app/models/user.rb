bcb08cdefb238b0622b378f14c8d4b3a9e8924b5
Frank Bender <JoeBender125@gmail.com>
2018-11-05 12:06:48 -0600

    Improved User model and added password with authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  before_save { email.downcase! } #self.email = email.downcase
   has_many :microposts
-  validates :name, length: { maximum: 40 }, presence: true
-  validates :email, presence: true
+  validates :name, length: { maximum: 50 }, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, length: { maximum: 255 }, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
